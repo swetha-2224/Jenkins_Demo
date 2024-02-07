60c68b1f119812633128c29f7415698db74bf607
swetha-2224 <158202524+swetha-2224@users.noreply.github.com>
2024-02-07 23:45:02 +0530

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,6 +1,7 @@
-if [ 1 = 1 ]
-then 
-echo "statement is true"
-else 
-echo "statement is false"
+Disk_usage=`df -kh | tail -4 | head -1 | awk -F " " '{print$(NF-1)}' | sed 's/%//g'`
+if [ $Disk_usage -gt 55 ]
+then
+echo "send an email alert"
+else
+echo "good condition"
 fi
