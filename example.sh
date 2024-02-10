6ce78c57a4b388fe0c3616cb278a65e32af717ce
swetha-2224 <158202524+swetha-2224@users.noreply.github.com>
2024-02-10 14:06:47 +0530

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,7 +1,1 @@
-Disk_usage=`df -kh | tail -4 | head -1 | awk -F " " '{print$(NF-1)}' | sed 's/%//g'`
-if [ $Disk_usage -gt 55 ]
-then
-echo "send an email alert"
-else
-echo "good condition"
-fi
+date
